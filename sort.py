d7e6c858a80be77ce5dab235177b2ef53a8d7a16
Christopher Nethercott <ccnethercott@gmail.com>
2022-08-05 20:58:02 +0100

    feat: add infinite loop & ifmain

--- sort.py
+++ sort.py
@@ -1,16 +1,33 @@
 from bs4 import BeautifulSoup
 
-with open('nongreen.html', 'r') as nongreen_raw:
-    nongreen_parsed = BeautifulSoup(nongreen_raw, 'html.parser')
+import time
+import datetime
 
-    lines = nongreen_parsed.body.find_all('tr', attrs={'class':'line'})
+NONGREEN_FILE_LOCATION = "/var/lib/xymon/www/nongreen.html"
+NONGREEN_SORTED_FILE_LOCATION = "/var/lib/xymon/www/nongreen_sorted.html"
 
-    lines = sorted(lines, key=lambda x: x.td.font.text)
+def main():
+    logging.basicConfig(level=logging.INFO)
 
-    table = nongreen_parsed.body.find_all('tbody')[1]
+    while True:
+        print(f'Writing sorted file at {datetime.datetime.now()}')
 
-    for line in lines:
-      table.append(line)
+        with open(NONGREEN_FILE_LOCATION, 'r') as nongreen_raw:
+            nongreen_parsed = BeautifulSoup(nongreen_raw, 'html.parser')
 
-    with open('nongreen_sorted.html', 'w') as nongreen_output:
-        nongreen_output.write(nongreen_parsed.prettify())
+            lines = nongreen_parsed.body.find_all('tr', attrs={'class':'line'})
+
+            lines = sorted(lines, key=lambda x: x.td.font.text)
+
+            table = nongreen_parsed.body.find_all('tbody')[1]
+
+            for line in lines:
+              table.append(line)
+
+            with open(NONGREEN_SORTED_FILE_LOCATION, 'w') as nongreen_output:
+                nongreen_output.write(nongreen_parsed.prettify())
+
+        time.sleep(60)
+
+if __name__ == '__main__':
+    main()
